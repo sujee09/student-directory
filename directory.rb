46d464ae832a5f9ce283ce39340c31752e663c67
Sujeevan Rajagopalan <sujee_09@hotmail.com>
2020-11-15 15:16:56 +0000

    added comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of students
 puts "The students of Villians Acamdemy"
 puts "------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,8 @@ puts "Freddy Kreuger"
 puts "The Joker"
 puts "Joffery Baratheon"
 puts "Norman Bates"
+# finally print the total no. students
 print "Overall, we have "
+# it's important the print() doesn't add new line charcter
 print 11
 puts " great students."

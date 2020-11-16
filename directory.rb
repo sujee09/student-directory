bb0b36bb98f17b0c050e3d8c71f715df0d5b0056
Sujeevan Rajagopalan <sujee_09@hotmail.com>
2020-11-16 15:40:58 +0000

    added csv file and .gitignore

--- directory.rb
+++ directory.rb
@@ -10,6 +10,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -27,6 +28,8 @@ def process(selection)
     show_students
   when "9"
     exit
+  when "3"
+    save_students
   else
   puts "I dont't know what you mean, try again"
   end
@@ -60,4 +63,14 @@ def print_footer
   puts "Overall, we have #{@students.count} great students. "
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end  
+  
 interactive_menu

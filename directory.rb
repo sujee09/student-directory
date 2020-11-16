a13cce17a61babf0e59f96f9bc39becc4709d961
Sujeevan Rajagopalan <sujee_09@hotmail.com>
2020-11-16 11:55:03 +0000

    further refactoring

--- directory.rb
+++ directory.rb
@@ -1,24 +1,34 @@
+@students = []
+
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+    print_menu
+    process(gets.chomp)
+  end
+end
 
-    selection = gets.chomp
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I dont't know what you mean, try again"
-    end
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+  puts "I dont't know what you mean, try again"
   end
 end
 
@@ -26,35 +36,28 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
  
-  students = []
   name = gets.chomp
    
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  
-  students
 end
-
+    
 def print_header
   puts "The students of Villians Acamdemy"
   puts "------------"
 end
 
-def print(students)
-students.each do |student|
+def print_student_list
+@students.each do |student|
   puts " #{student[:name]} (#{student[:cohort]} cohort)" 
  end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students. "
+def print_footer
+  puts "Overall, we have #{@students.count} great students. "
 end
 
 interactive_menu
-students = input_students
-print_header
-print(students)
-print_footer(students)

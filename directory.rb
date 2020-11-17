41d8373456c2fcf308beaa7d35108903c0c6d830
Sujeevan Rajagopalan <sujee_09@hotmail.com>
2020-11-17 10:35:47 +0000

    added csv from startup

--- directory.rb
+++ directory.rb
@@ -3,7 +3,7 @@
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -42,12 +42,12 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
  
-  name = gets.chomp
+  name = STDIN.gets.chomp
    
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
     
@@ -76,8 +76,8 @@ def save_students
   file.close
 end  
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = students.csv)
+  file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -85,4 +85,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+      puts "Loaded #{@students.count} from #{filename}"
+  else 
+    puts "Sorry #{filename} doesn't exist"
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
